a446300850b2611e70f219bded9105572586ab70
Greg Williams <williams.greg@gmail.com>
2020-12-06 09:37:12 -0800

    refactor(Day 6): Consilidated logic to share between common and unique answer sets

--- src/advent2020/day6/day6.kt
+++ src/advent2020/day6/day6.kt
@@ -19,31 +19,23 @@ a
 b"""
 
 typealias Answers = Set<Char>
-fun String.collectGroupAnswers(): MutableList<Answers> = splitToSequence("\n")
-    .fold(mutableListOf(mutableSetOf())) { groupList, line ->
+fun String.collectGroupAnswers(defaultAnswers:Answers = setOf(), combine: (Answers, Iterable<Char>) -> Answers): MutableList<Answers> = splitToSequence("\n")
+    .fold(mutableListOf(defaultAnswers)) { groupList, line ->
         val groupAnswers = if (line.isEmpty()) {
-            setOf()
+            defaultAnswers
         } else {
             val answers = groupList.removeAt(groupList.lastIndex)
-            answers.plus(line.asIterable())
+            combine(answers, line.asIterable())
         }
         groupList.also { it.add(groupAnswers) }
     }
 
 fun defaultAnswers(): Answers = 'a'.rangeTo('z').toMutableSet()
-fun String.collectGroupUnanimousAnswers(): MutableList<Answers> = splitToSequence("\n")
-    .fold(mutableListOf(defaultAnswers())) { groupList, line ->
-        val groupAnswers = if (line.isEmpty()) {
-            defaultAnswers()
-        } else {
-            val groupAnswers = groupList.removeAt(groupList.lastIndex)
-            groupAnswers.intersect(line.asIterable())
-        }
-        groupList.also { it.add(groupAnswers) }
-    }
+fun String.collectCommonGroupAnswers() = collectGroupAnswers { answers, line -> answers.plus(line) }
+fun String.collectUnanimousGroupAnswers() = collectGroupAnswers(defaultAnswers()) { answers, line -> answers.intersect(line) }
 
 fun main() {
-    val testAnswers = sample.collectGroupAnswers()
+    val testAnswers = sample.collectCommonGroupAnswers()
     assertEquals(setOf('a', 'b', 'c'), testAnswers[0])
     assertEquals(setOf('a', 'b', 'c'), testAnswers[1])
     assertEquals(setOf('a', 'b', 'c'), testAnswers[2])
@@ -51,13 +43,13 @@ fun main() {
     assertEquals(setOf('b'), testAnswers[4])
     assertEquals(11, testAnswers.sumBy { it.size })
 
-    val pt1 = input.collectGroupAnswers().sumBy { it.size }
+    val pt1 = input.collectCommonGroupAnswers().sumBy { it.size }
     println(pt1)
     assertEquals(6585, pt1)
 
-    assertEquals(6, sample.collectGroupUnanimousAnswers().sumBy { it.size })
+    assertEquals(6, sample.collectUnanimousGroupAnswers().sumBy { it.size })
 
-    val pt2 = input.collectGroupUnanimousAnswers().sumBy { it.size }
+    val pt2 = input.collectUnanimousGroupAnswers().sumBy { it.size }
     println(pt2)
     assertEquals(3276, pt2)
 }
