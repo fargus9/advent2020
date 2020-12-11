de59b3ec6bac80113e7a901664126cbf215dae84
Greg Williams <williams.greg@gmail.com>
2020-12-11 06:30:51 -0800

    fix(Day 10): changed to using stable key for cache, but the algorithm is till too suboptimal. Also tried a leaner algorithm for calculating but it hasn't worked out.

--- src/advent2020/day10/day10.kt
+++ src/advent2020/day10/day10.kt
@@ -16,15 +16,28 @@ fun Jolts.simpleJoltDistribution(): JoltDistribution = windowed(2)
 fun JoltDistribution.product() = asSequence().map { (_, count) -> count }.reduce { total, count -> total * count }
 
 typealias Joltage = Array<Int>
-val matchCache = mutableMapOf<Pair<Int, Int>, Long>()
-fun findValidCombinations(consider: Joltage, considering: Int, target: Int): Long = matchCache.getOrPut(considering to target) {
+val cache = mutableMapOf<String, Long>()
+fun findValidCombinations(consider: Joltage, considering: Int, target: Int): Long = cache.getOrPut("$considering:$target") {
     val leastPossible = consider.indexOfFirst { (target - it) <= 3 }
     if (leastPossible == -1) { return if (target <= 3) 1L else 0L }
-    return (leastPossible..considering).asSequence()
+    return considering.downTo(leastPossible).asSequence()
         .map { it - 1 to consider[it] }
         .map { (nowConsidering, newTarget) -> findValidCombinations(consider, nowConsidering, newTarget) }
         .sum() + if (target <= 3) 1L else 0L
 }
+
+fun findFromBottom(consider: Joltage, target: Int): Long = sequence {
+    var considering = 0
+    var compare = 0
+    yield(1)
+    while (considering < consider.count() && compare < target) {
+        val values = consider.asSequence().drop(considering).takeWhile { it - compare <= 3 }
+        val count = values.count().takeUnless { it == 1 } ?: 0
+        yield(count.toLong())
+        considering += 1
+        compare = values.firstOrNull() ?: target
+    }
+}.reduce { total, count -> total + count }
 
 fun main() {
     val smallJolts = sample1.toJolts()
@@ -40,14 +53,18 @@ fun main() {
     assertEquals(2346, pt1Output)
 
     val smallCombinations = with (smallJolts.toList().toTypedArray()) {
+        //assertEquals(8L, findFromBottom(this, last() + 3))
         findValidCombinations(this, lastIndex, last() + 3)
     }
     assertEquals(8L, smallCombinations)
+    cache.clear()
 
     val largeCombinations = with (largeJolts.toList().toTypedArray()) {
+        ///assertEquals(19208L, findFromBottom(this, last() + 3))
         findValidCombinations(this, lastIndex,last() + 3)
     }
     assertEquals(19208L, largeCombinations)
+    cache.clear()
 
     val pt2output = with (inputJolts.toList().toTypedArray()) {
         findValidCombinations(this, lastIndex,last() + 3)
