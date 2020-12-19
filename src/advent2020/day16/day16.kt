71ab3551b3d8deaeb516adfa7bf20a2394330828
Greg Williams <williams.greg@gmail.com>
2020-12-19 08:16:15 -0800

    solution(Day 16): Took some clarification to get the logic straight but I arrived at a solution

--- src/advent2020/day16/day16.kt
+++ src/advent2020/day16/day16.kt
@@ -3,17 +3,15 @@ package advent2020.day16
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 
-data class RuleBounds(var lower: Int, var gapLower: Int, var gapUpper: Int, var upper: Int)
-
-typealias Rules = Sequence<LongRange>
 fun String.toRules() = lineSequence()
     .fold(sequenceOf<LongRange>()) { bounds, line ->
         val newBounds = "^[a-z ]+: ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)$".toRegex().find(line)!!.destructured
             .let { (l, ml, mu, up) -> sequenceOf(l.toLong()..ml.toLong(), mu.toLong()..up.toLong()) }
         bounds + newBounds
     }
-fun String.toTickets(): Sequence<Sequence<Long>> = lineSequence()
-    .map { it.splitToSequence(",").map { value -> value.toLong() } }
+
+fun String.toTicket(): Sequence<Long> = splitToSequence(",").map { value -> value.toLong() }
+fun String.toTickets(): Sequence<Sequence<Long>> = lineSequence().map { it.toTicket() }
 
 fun sumOfInvalidValuesForNearbyTickets(rules: String, nearbyTickets: String): Long {
     val bounds = rules.toRules()
@@ -23,30 +21,40 @@ fun sumOfInvalidValuesForNearbyTickets(rules: String, nearbyTickets: String): Lo
         .reduce { total, value -> total + value }
 }
 
-fun findColumnsForRules(rulesInput: String, nearbyTickets: String) {
-    val validBounds = rulesInput.toRules()
-    val validRanges = nearbyTickets.toTickets()
-        .filterNot { it.any { value -> validBounds.any { rule -> value !in rule } } }
-        .fold(Array(validBounds.count() / 2) { LongArray(2) { if (it == 0) Long.MAX_VALUE else Long.MIN_VALUE }}) {
-                ranges, values ->
-            values.forEachIndexed { index, value ->
-                ranges[index].let {
-                    if (value < it[0]) { it[0] = value }
-                    if (value > it[1]) { it[1] = value }
+fun findColumnsForRules(rulesInput: String, nearbyTickets: String, ticket: Sequence<Long>): IntArray? {
+    val rules = rulesInput.toRules().chunked(2).map { it.toTypedArray() }.toList().toTypedArray()
+    val validTickets = nearbyTickets.toTickets()
+        .filter { it.all { value -> rules.any { rule -> rule.any { range -> value in range } } } }
+
+    val columnCount = ticket.count()
+    val possibleMatches = validTickets.plus(sequenceOf(ticket))
+        .fold(Array(rules.size) { BooleanArray(columnCount) { true } }) { possibilities, values ->
+            values.forEachIndexed { valueIndex, value ->
+                rules.forEachIndexed { rulesIndex, rule ->
+                    possibilities[rulesIndex][valueIndex] = possibilities[rulesIndex][valueIndex] && rule.any { value in it }
                 }
             }
-            ranges
+            possibilities
         }
-    val rules = validBounds.chunked(2).map { it.toTypedArray() }.toList().toTypedArray()
-    val possibleMatches = rules.foldIndexed(Array(rules.size) { BooleanArray(rules.size) { true } }) { ruleIndex, possible, rule ->
-        val validity = possible[ruleIndex]
-        validRanges.forEachIndexed { valueIndex, (lower, upper) ->
-            validity[valueIndex] = rule.all { range -> lower in range && upper in range }
-        }
-        possible
-    }
-    val valid = possibleMatches.indices.first { possibleMatches.all { possibilities -> possibilities[it] } }
 
+    return findCombination(IntArray(rules.size) { -1 }, possibleMatches)
+}
+
+fun findCombination(given: IntArray, possibilities: Array<BooleanArray>): IntArray? {
+    if (given.none { it == -1 }) return given
+    val undecidedRules = possibilities.indices.asSequence().filter { given[it] == -1 }
+    val undecidedValues = possibilities.first().indices.asSequence().filterNot { given.contains(it) }
+    if (undecidedValues.any { possibilities.count { column -> column[it] } == 0 }) return null
+    undecidedValues.sortedBy { possibilities.count { column -> column[it] } }
+        .forEach { trialValue ->
+            undecidedRules.forEach { trialRule ->
+                if (possibilities[trialRule][trialValue]) {
+                    findCombination(given.copyOf().also { next -> next[trialRule] = trialValue }, possibilities)
+                        ?.let { found -> return@findCombination found }
+                }
+            }
+        }
+    return null
 }
 
 fun main() {
@@ -55,5 +63,18 @@ fun main() {
     val pt1Output = sumOfInvalidValuesForNearbyTickets(rules, nearbyTickets)
     assertEquals(19087, pt1Output)
 
+    val sampleColumns = findColumnsForRules(sampleRules, sampleNearbyTickets, sampleTicket.toTicket())!!
+    assertEquals(1, sampleColumns[0])
+    assertEquals(0, sampleColumns[1])
+    assertEquals(2, sampleColumns[2])
 
+    val ruleColumns = findColumnsForRules(rules, nearbyTickets, ticket.toTicket())!!
+    val yourTicket = ticket.split(",")
+    val departingRules = rules.lineSequence().mapIndexed { index: Int, it: String ->
+        if (it.startsWith("departure")) index else -1 }
+        .filterNot { it == -1 }
+    val pt2Output = departingRules.map { yourTicket[ruleColumns[it]].toLong() }
+        .reduce { total, column -> total * column }
+
+    assertEquals(1382443095281, pt2Output)
 }
